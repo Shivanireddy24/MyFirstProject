a12ce7ccf87e9905e4e01aa91e039e186f4c5ecb
annaram.reddy <annaram.reddy@HYDPCM329443L.ad.infosys.com>
2020-08-07 21:28:00 +0530

    First Commit

---
+++ MyFirstProject/src/Myclass.java
@@ -0,0 +1,24 @@
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		
+		int input=10;
+		
+		int finalResult =summation(input);
+		
+		System.out.println(finalResult);
+		
+		// TODO Auto-generated method stub
+
+	}
+	public static int summation(int n) {
+		int result = (n*(n+1))/2;
+		
+		return result;
+		
+		
+	}
+	
+
+}
